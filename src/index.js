89a5fcfbaac97c2eed277e49b0cfd826ec466e7c
shawn <shawnsmith0024@gmail.com>
2022-12-20 10:22:05 -0500

    fetched movies, rendered functions

---
+++ src/index.js
@@ -0,0 +1,54 @@
+//fetch movies 
+fetch('http://localhost:3000/movies')
+    .then(resp => resp.json())
+    .then(movies => {
+        renderMovies(movies)
+        renderOneMovie(movies[0])
+        handleBloodForm()
+    })
+    
+
+    //render functions
+    function renderMovies(movies){
+        movies.forEach(movie => {
+            let imageTag = document.createElement('img')
+            imageTag.src = movie.image
+            let pictureContainer = document.querySelector('#movie-list')
+            pictureContainer.append(imageTag)
+            imageTag.addEventListener('click', () => renderOneMovie(movie))
+        })
+    }
+
+    //show the details of the FIRST movie in the dataset 
+    function renderOneMovie(movie){
+        let title = document.querySelector('#title')
+        title.textContent = movie.title
+        let year = document.getElementById('year-released')
+        year.textContent = movie.release_year
+        let desc = document.getElementById('description')
+        desc.textContent = movie.description
+        let image = document.querySelector('#detail-image')
+        image.src = movie.image
+        let blood = document.querySelector('#amount')
+        blood.textContent = movie.blood_amount
+        let watchedButton = document.querySelector('#watched')
+        watchedButton.textContent = movie.watched ? "Watched": "Unwatched"
+        watchedButton.addEventListener('click', () => toggleWatchedButtton(movie))
+    }
+
+    function toggleWatchedButtton(movie){
+        movie.watched = !movie.watched
+        let watchedButton = document.querySelector('#watched')
+        watchedButton.textContent = movie.watched ? 'Watched': 'Unwatched'
+        console.log(movie)
+    }
+    
+    function handleBloodForm(){
+        let bloodForm = document.querySelector('#blood-form')
+        bloodForm.addEventListener('submit',() => {
+            e.preventDefault()
+            console.log(e)
+        })
+    }
+    
+        
